8abd7c8507d7d5ff348d544f2a853e7af2634fbc
Tassuo7 <abdelhamid.jack@gmail.com>
2023-09-24 19:39:46 -0700

    add condition

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -51,6 +51,10 @@ def number_template(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     """Display a HTML page if n is even|odd"""
+    if n % 2 != 0:
+        num = "odd"
+    else:
+        num = "even"
     return render_template("6-number_odd_or_even.html", n=n)
 
 
